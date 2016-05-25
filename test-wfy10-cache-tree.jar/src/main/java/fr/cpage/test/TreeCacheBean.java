db0f41307ed76076f0ab87f42d963788060f57f5
Guillot Julien <julien.guillot@cpage.fr>
2016-05-25 08:51:03 +0200

    Add configuration debug

--- test-wfy10-cache-tree.jar/src/main/java/fr/cpage/test/TreeCacheBean.java
+++ test-wfy10-cache-tree.jar/src/main/java/fr/cpage/test/TreeCacheBean.java
@@ -6,6 +6,7 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
 import org.infinispan.Cache;
+import org.infinispan.cache.impl.AbstractDelegatingCache;
 import org.infinispan.tree.TreeCacheFactory;
 
 @Startup
@@ -16,7 +17,27 @@ public class TreeCacheBean {
 
   @PostConstruct
   public void onStartup() {
+    debugInfo();
     new TreeCacheFactory().createTreeCache(this.testBaseCache);
+  }
+
+  private void debugInfo() {
+    try {
+      final AbstractDelegatingCache delegating = (AbstractDelegatingCache) this.testBaseCache;
+      final Cache delegate = delegating.getDelegate();
+      System.out.println("*****************");
+      System.out.println("Cache Class:" + delegate.getClass());
+      System.out.println("Batching: " + this.testBaseCache.getCacheConfiguration()
+          .invocationBatching()
+          .enabled());
+      System.out.println("Max entries: " + this.testBaseCache.getCacheConfiguration().eviction().maxEntries());
+      if (delegate instanceof org.infinispan.cache.impl.CacheImpl) {
+        final org.infinispan.cache.impl.CacheImpl cacheImpl = (org.infinispan.cache.impl.CacheImpl) delegate;
+        System.out.println("Batch Container:" + cacheImpl.getBatchContainer());
+      }
+      System.out.println("*****************");
+    } catch (final Throwable t) {
+    }
   }
 
 }
